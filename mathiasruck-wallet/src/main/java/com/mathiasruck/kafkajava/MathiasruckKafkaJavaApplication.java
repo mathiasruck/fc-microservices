b212910f96f095eb9e7a503a4a2876f7ba60f50a
Mathias <mathiasruck@gmail.com>
2024-03-16 21:33:21 +0200

    adding java project to connect to Kafka

---
+++ mathiasruck-wallet/src/main/java/com/mathiasruck/kafkajava/MathiasruckKafkaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mathiasruck.kafkajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathiasruckKafkaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathiasruckKafkaJavaApplication.class, args);
+    }
+
+}
---
+++ mathiasruck-wallet/src/test/java/com/mathiasruck/kafkajava/MathiasruckKafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathiasruck.kafkajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathiasruckKafkaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
